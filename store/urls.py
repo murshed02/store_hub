2366471fcd27a5439ab637f1e24f5ccd409cf7a2
murshed02 <khirallahmurshed979@gmail.com>
2024-09-07 04:12:33 +0300

    ADD

--- store/urls.py
+++ store/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
    path('Category/',views.category , name='category') , 
    path('Category/Store/',views.store,name='store') , 
    #path('Home/Cart/', views.cart , name= 'cart') ,
-   path('/signup/', views.signup,name= 'signup') , 
-   path('/Login/',views.login,name='login') ,
+   path('signup/', views.signup,name= 'signup') , 
+   path('Login/',views.login,name='login') ,
    path('admin/',views.admin , name= 'admin') , 
    path('CreateStore', views.create_store , name= 'create_store')
 ]
